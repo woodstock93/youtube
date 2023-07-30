39ad706e71db9e8ef196813d7ba2f49e1096bba3
woodstock93 <woodstock93@naver.com>
2023-07-31 08:53:39 +0900

    ADD: router controller 분리

--- src/server.js
+++ src/server.js
@@ -1,30 +1,25 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRotuer";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 // express function
 const PORT = 4000;
 const app = express();
 const morganLogger = morgan("dev");
 
-// request, respond given by express
-const home = (req, res, next) => {
-    console.log("Home Home Home ...")
-    return res.send("Home Page");
-}
-const login = (req, res) => {
-    console.log("Login Login Login ...")
-    return res.send("Login Page");
-}
-const handleProtected = (req, res) => {
-    return res.send("");
-}
-
-// middleWare + URL
+// middleWare
 app.use(morganLogger);
-app.get("/", home);
-app.get("/protected", handleProtected)
-app.get("/login", login)
+
+// Rotuer 정의 => 함수 정의 => Router.HttpMethod(Path, callBackFuntion)
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
 
 // Server Composition
 const handleListening = () => console.log(`Server Listening on port http://localhost:${PORT}`)
 app.listen(4000, handleListening)
+
